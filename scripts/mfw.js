5c6040a6f9d434791fb3da9bf47d9a24693a02fe
Patrick Springstubbe <stubbe@umich.edu>
2020-08-18 15:01:26 -0400

    jquery deprecated function updates

--- scripts/mfw.js
+++ scripts/mfw.js
@@ -45,7 +45,7 @@
 
         // IE HTML5 VIDEO DOWNLOAD CHECK
         // ie can fail some partial downloads of video files
-         $('video').error(function(){
+         $('video').on('error', function(){
             // if we get code 4, formats are not supported
             // anything else try and reload it, up to 4 times
             if( ($(this).get(0).error.code != 4) && ($(this).data('attempt') < 4) ) {
@@ -60,7 +60,7 @@
         });
 
         // wait for assets to load
-        $(window).load(function(){
+        $(window).on('load', function(){
             // RESPONSIVE MENU
             var mainMenuWrap = $('#zone-header-menu .mfwMenu');
             var mainMenuList = mainMenuWrap.find('.header-menu');
