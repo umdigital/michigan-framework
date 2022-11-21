812e124a4c98786d32e36117327ece62857ee5c1
Patrick Springstubbe <stubbe@umich.edu>
2022-11-21 09:25:26 -0500

    php error fix

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ if( is_front_page() ) {
     get_template_part( 'templates/homepage' );
 }
 else {
-    get_template_part( 'templates/posts', ($post->post_type != 'posts' ? $post->post_type : null) );
+    get_template_part( 'templates/posts', (@$post->post_type != 'posts' ? $post->post_type : null) );
 }
